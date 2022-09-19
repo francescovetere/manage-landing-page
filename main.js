12bddb7a47c97819996189f6fb323c6145149f48
francescovetere <francescovetere1997@gmail.com>
2022-09-19 22:39:38 +0200

    Fixed typo

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
-  console.log("het");
   primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true);
